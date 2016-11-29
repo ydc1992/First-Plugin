8d6af1dd63b42bc8640435a12fc8ae63a4fae03a
ydc1992 <ydc1992@gmail.com>
2016-11-29 22:42:13 +0800

    update

--- MyPlugin.cpp
+++ MyPlugin.cpp
@@ -1,170 +1,127 @@
 // MyPlugin.cpp : 定义 DLL 应用程序的导出函数。
 
 #include "stdafx.h"
-#include "plugin.hpp"
 #include <tchar.h>
 #include <malloc.h>
 #include <stdio.h>
 
-bool DUMPFILE(LPTSTR lpFileName);
+#define MVV_1 0
+#define MVV_2 8
+#define MVV_3 15
+#define MVV_4 0
+#define MVV_4a ""
 
+#define FILENAMEPTR(p) (p).FindData.cFileName
 
-//GUID
-
-// {1D228F40-FE40-4663-92D9-EB184E455AF1}
-static const GUID guid_PluginMenu =
-{ 0x1d228f40, 0xfe40, 0x4663,{ 0x92, 0xd9, 0xeb, 0x18, 0x4e, 0x45, 0x5a, 0xf1 } };
-
-// {1C6B0907-0B96-47C4-A24B-36A9B184AF25}
+// {096AC917-738A-45A1-8F6A-3E43C523236C}
 static const GUID guid_PluginGuid =
-{ 0x1c6b0907, 0xb96, 0x47c4,{ 0xa2, 0x4b, 0x36, 0xa9, 0xb1, 0x84, 0xaf, 0x25 } };
+{ 0x96ac917, 0x738a, 0x45a1,{ 0x8f, 0x6a, 0x3e, 0x43, 0xc5, 0x23, 0x23, 0x6c } };
+
+// {2B829E4A-7707-422F-B063-0D7537130951}
+static const GUID guid_PluginMenu =
+{ 0x2b829e4a, 0x7707, 0x422f,{ 0xb0, 0x63, 0xd, 0x75, 0x37, 0x13, 0x9, 0x51 } };
 
 
-
-
-#define SAFEFREE(p)  if(!(p)){free((p));(p) = NULL;}
-#define RETURNULL(p) if(!(p))return NULL;
-#define RTTYPE(p,q)  if(p)return (q);
-#define SAFECLOSEHANDLE(p,q) if(!p){CloseHandle(p);return (q);}
-
-//全局变量
-AnalyseInfo * gpAnalyInfo = NULL;
-//psi 和 fsf 不能是指针类型，否则使用该变量会报错
 PluginStartupInfo psi;
 FarStandardFunctions fsf;
 
-
-//函数声明
-void WINAPI GetOpenPluginInfo(OpenPanelInfo *Info);
-
-//************************************
-// Method:    GetGlobalInfoW
-// FullName:  GetGlobalInfoW
-// Access:    public
-// Returns:   void WINAPI
-// Qualifier:  Far最先加载这个函数，插件菜单中F3显示的内容，导出函数,Far第一个调用的函数
-// Parameter: struct GlobalInfo * Info
-//************************************
-void WINAPI GetGlobalInfoW(GlobalInfo *Info)
+void WINAPI GetGlobalInfoW(struct GlobalInfo *Info)
 {
-	Info->MinFarVersion = MAKEFARVERSION(
-		FARMANAGERVERSION_MAJOR,
-		FARMANAGERVERSION_MINOR,
-		FARMANAGERVERSION_REVISION,
-		FARMANAGERVERSION_BUILD,
-		FARMANAGERVERSION_STAGE);
 
-	Info->StructSize = sizeof(GlobalInfo);
-	Info->Author = L"Ken";
-	Info->Description = L"My First Plugin";
-	Info->Title = L"My First Plugin";
+	Info->MinFarVersion = /*FARMANAGERVERSION*/
+		MAKEFARVERSION(
+			FARMANAGERVERSION_MAJOR,
+			FARMANAGERVERSION_MINOR,
+			FARMANAGERVERSION_REVISION,
+			FARMANAGERVERSION_BUILD,
+			FARMANAGERVERSION_STAGE);
+
+	Info->Version = MAKEFARVERSION(MVV_1, MVV_2, MVV_3, ((MVV_1 % 100) * 100000) + (MVV_2 * 1000) + (MVV_3 * 10) + (MVV_4 % 10), VS_RELEASE);
+
 	Info->Guid = guid_PluginGuid;
+	Info->Title = L"First Plugin";
+	Info->Description = L"PE Import/Export browser";
+	Info->Author = L"ConEmu.Maximus5@gmail.com";
 }
 
-//************************************
-// Method:    SetStartupInfoW
-// FullName:  SetStartupInfoW
-// Access:    public
-// Returns:   void WINAPI
-// Qualifier: 设置插件启动信息，很重要。导出函数，Far第二个调用的函数
-// Parameter: const PluginStartupInfo * Info
-//************************************
-void WINAPI SetStartupInfoW(const PluginStartupInfo *Info)
+void WINAPI GetPluginInfoW(struct PluginInfo *pi)
 {
-	//传递给全局变量，以便在后面调用。
+	static wchar_t* szMenu = L"Impex1";
+
+	pi->StructSize = sizeof(PluginInfo);
+	pi->Flags = PF_FULLCMDLINE;
+	pi->CommandPrefix = szMenu;
+
+	// 显示菜单
+	pi->PluginMenu.Count = 1;
+	pi->PluginMenu.Guids = &guid_PluginMenu;
+	pi->PluginMenu.Strings = &szMenu;
+
+}
+
+void WINAPI SetStartupInfoW(struct PluginStartupInfo *Info) {
 	psi = *Info;
 	fsf = *Info->FSF;
 	psi.FSF = &fsf;
 }
 
-//************************************
-// Method:    GetPluginInfoW
-// FullName:  GetPluginInfoW
-// Access:    public
-// Returns:   void WINAPI
-// Qualifier: 获取插件信息，导出函数
-// Parameter: struct PluginInfo * Info
-//************************************
-void WINAPI  GetPluginInfoW(PluginInfo *Info)
-{
-	static wchar_t* szMenu;
-	szMenu = _T("First Plugin");
-
-	Info->StructSize = sizeof(PluginInfo);
-	Info->Flags = PF_FULLCMDLINE;
-	Info->CommandPrefix = szMenu;
-
-	//插件菜单，要在插件菜单中显示菜单，必须初始化这三项，缺一不可，不然会报错
-	Info->PluginMenu.Count = 1;
-	Info->PluginMenu.Guids = &guid_PluginMenu;
-	Info->PluginMenu.Strings = &szMenu;
-}
-
-//************************************
-// Method:    GetFullPath
-// FullName:  GetFullPath
-// Access:    public
-// Returns:   LPTSTR
-// Qualifier: 获得当前选中项的文件完整路径
-//************************************
-LPTSTR GetCurrentSelectItemFullPath()
-{
-	PanelInfo pi;
-	wchar_t* lpFullName = NULL;
-	//1. 获取面板信息
-	RTTYPE(!psi.PanelControl(PANEL_ACTIVE, FCTL_GETPANELINFO, 0, &pi), NULL);
-
-	RTTYPE(!(pi.PanelType == PTYPE_FILEPANEL && pi.ItemsNumber > 0), NULL);
-	RTTYPE(pi.CurrentItem < 0, NULL);
-	//2. 获取面板项
-
-	//2.1 获得其size
-	INT_PTR size = psi.PanelControl(PANEL_ACTIVE, FCTL_GETPANELITEM, pi.CurrentItem, NULL);
-	RETURNULL(size);
-	//2.2 分配面板项空间
-	PluginPanelItem* ppi = (PluginPanelItem*)malloc(size);
-	RETURNULL(ppi);
-	// 2.3 再次调用，获得选中项详细信息
-	FarGetPluginPanelItem FGPPI = { sizeof(FarGetPluginPanelItem),size,ppi };
-	psi.PanelControl(PANEL_ACTIVE, FCTL_GETPANELITEM, pi.CurrentItem, &FGPPI);
-
-	// 3.文件完整路径
-	wchar_t* lpFileName = (wchar_t*)ppi->FileName;
-	RTTYPE((lpFileName && *lpFileName && _tcscmp(lpFileName, L"..") == 0), NULL);
-
-	size_t nLen = fsf.ConvertPath(CPM_FULL, lpFileName, NULL, 0);
-	RETURNULL(nLen);
-	lpFullName = (wchar_t*)malloc(nLen*2);
-	fsf.ConvertPath(CPM_FULL, lpFileName, lpFullName, nLen);
-	SAFEFREE(ppi);
-	return lpFullName;
-}
-
-//************************************
-// Method:    OpenW
-// FullName:  OpenW
-// Access:    public
-// Returns:   HANDLE WINAPI
-// Qualifier:启动插件，开始工作了
-// Parameter: const struct OpenInfo * Info
-//************************************
-HANDLE WINAPI OpenW(const struct OpenInfo *Info)
-{
+HANDLE WINAPI OpenW(const struct OpenInfo *Info) {
 	OPENFROM OpenFrom = Info->OpenFrom;
-	wchar_t* pszname = (wchar_t*)malloc(MAX_PATH*2);
+	INT_PTR Item = Info->Data;
 
-	//从插件菜单打开
-	if (OpenFrom == OPEN_PLUGINSMENU)
+	const HANDLE PanelStop = PANEL_STOP;
+	const HANDLE PanelNone = NULL;
+
+	PluginPanelItem* item = NULL;
+	LPCTSTR pszFileName = NULL;
+
+	HANDLE hPlugin = PanelNone;
+	if (OpenFrom == OPEN_PLUGINSMENU) {
+		PanelInfo pi = {};
+		pi.StructSize = sizeof(pi);
+		// 获取面板信息
+		if (psi.PanelControl(INVALID_HANDLE_VALUE, FCTL_GETPANELINFO, 0, &pi)) {
+			//是文件面板并且列表不为空
+			if (pi.PanelType == PTYPE_FILEPANEL && pi.ItemsNumber > 0) {  
+				if (pi.CurrentItem < 0)
+					return NULL;
+
+				//获取鼠标指向的那一项
+				INT_PTR nSize= psi.PanelControl(INVALID_HANDLE_VALUE, FCTL_GETPANELITEM, pi.CurrentItem, NULL);
+
+				item = (PluginPanelItem*)calloc(nSize, 1);
+				FarGetPluginPanelItem gpi = { sizeof(gpi),nSize,item };
+				//得到面板项的文件信息
+				psi.PanelControl(INVALID_HANDLE_VALUE, FCTL_GETPANELITEM, pi.CurrentItem, &gpi);
+ 				pszFileName = item->FileName;
+
+				TCHAR* pszFull = NULL;
+				int nLen = 0;
+				if(pszFileName && *pszFileName && _tcscmp(pszFileName, _T("..")) != 0){
+					//先获取文件名长度,以免缓冲区不足。
+				    nLen = fsf.ConvertPath(CPM_FULL, pszFileName, NULL, 0);
+					if (nLen > 0) {
+						pszFull = (TCHAR*)calloc(nLen, sizeof(TCHAR));
+						fsf.ConvertPath(CPM_FULL, pszFileName, pszFull, nLen);
+						if (pszFileName) {
+					
+						}
+					}
+				}
+
+			}
+		}
+	
+	}
+	else if(OpenFrom == OPEN_ANALYSE) //打开的文件的内容
 	{
-		pszname = GetCurrentSelectItemFullPath();
+		
+	}
+	else if (OpenFrom ==OPEN_COMMANDLINE)
+	{
+
 	}
 
-	return NULL;
-}
 
-void WINAPI GetOpenPluginInfo(OpenPanelInfo *Info) 
-{
-	Info->CurDir = L"";
-	Info->DescrFiles = NULL;
-
+	return hPlugin;
 }
--- Win32Project1/stdafx.h
+++ Win32Project1/stdafx.h
@@ -8,8 +8,14 @@
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
-// Windows 头文件:
+// Windows 头文件: 
 #include <windows.h>
-#include "plugin.hpp"
+
+// C 运行时头文件
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
 
 // TODO:  在此处引用程序需要的其他头文件
